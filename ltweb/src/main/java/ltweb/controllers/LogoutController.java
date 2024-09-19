762cd4a3f6b0531a791ab6232501e2ebfb8929c7
Tuấn Kiệt <tuankiet18032004@gmail.com>
2024-09-19 13:45:20 +0700

    hoan thanh

---
+++ ltweb/src/main/java/ltweb/controllers/LogoutController.java
@@ -0,0 +1,35 @@
+package ltweb.controllers;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet(urlPatterns = "/logout")
+public class LogoutController extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(req, resp);
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = req.getSession();
+		if(session.getAttribute("account") != null) {
+			session.removeAttribute("account");
+			session.invalidate();
+			resp.sendRedirect(req.getContextPath() + "/login");
+		}
+	}
+
+}
